e7034d051bb7f5cb2ef0e2e0cbfbe4c1261b1cb8
pranavbhadane-create <pranav.bhadane@aessefin.it>
2026-06-04 17:24:07 +0200

    gh pages deploy + images

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['worshiper-visibly-eastcoast.ngrok-free.dev'],
+  },
 })
